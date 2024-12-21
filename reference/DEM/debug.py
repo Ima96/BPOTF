7e994a3a9f6b1d94a3368da29fc88e1466e9c4ca
Imanol Etxezarreta <ietxezarretam@gmail.com>
2024-12-21 12:05:29 +0100

    Update debug script to include memory usage stats, timming stats and general info.

--- reference/DEM/debug.py
+++ reference/DEM/debug.py
@@ -1,4 +1,5 @@
 import scipy.io as sio
+from scipy.sparse import csc_matrix
 conts = sio.loadmat('testBBCLNmap144_12_12_12rounds_p_001.mat')
 from BPOTF2 import UFCLN
 # from BPOTFog import UFCLN as UFCLN2
@@ -22,8 +23,13 @@ else:
 import BPOTF
 from timeit import default_timer as timer
 
+import os
+import psutil
+
 from BPOTF import __version__ as bpotf_version
 print(f"BPOTF version is v{bpotf_version}")
+
+PRINTING = False
 
 dem = conts['dem']
 priors = conts['priors']
@@ -31,10 +37,34 @@ transf_M = conts['transfMatFull']
 H_phen = conts['Hphen']
 obs = conts['obs']
 hz = conts['hz']
-code, A_list, B_list = create_bivariate_bicycle_codes(12, 6, [3], [1,2], [1,2], [3])
-d = 12
-p = 0.0003
-NMC = 10**6
+
+BB_TYPE = 288
+if BB_TYPE == 72:
+    # [72, 12, 6] último número es el numero de rondas
+    code, A_list, B_list = create_bivariate_bicycle_codes(6, 6, [3], [1,2], [1,2], [3])
+    d = 6
+    transfer_mat = sio.loadmat('transfermatrices/transferMatrixcodel6m6.mat')['transfMat']
+elif BB_TYPE == 108:
+    # [108, 8, 10]
+    code, A_list, B_list = create_bivariate_bicycle_codes(9, 6, [3], [1,2], [1,2], [3])
+    d = 10
+    transfer_mat = sio.loadmat('transfermatrices/transferMatrixcodel9m6.mat')['transfMat']
+elif BB_TYPE == 144:
+    # [144, 12, 12]
+    code, A_list, B_list = create_bivariate_bicycle_codes(12, 6, [3], [1,2], [1,2], [3])
+    d = 12
+    transfer_mat = sio.loadmat('transfermatrices/transferMatrixcodel12m6.mat')['transfMat']
+elif BB_TYPE == 288:
+    #  [288, 12, 18]
+    code, A_list, B_list = create_bivariate_bicycle_codes(12, 12, [3], [2,7], [1,2], [3])
+    d = 18
+    # transfer_mat = sio.loadmat('transfermatrices/BB288TransfDemsObs.mat')['transfMat']
+    transfer_mat = csc_matrix(sio.loadmat('transfermatrices/BB288CSC.mat')['transfMat'])
+else:
+    raise Exception("No such option!")
+
+p = 0.004
+NMC = 10**2
 
 circuit = build_circuit(code, A_list, B_list, 
                         p=p, # physical error rate
@@ -45,9 +75,7 @@ circuit = build_circuit(code, A_list, B_list,
 dem = circuit.detector_error_model()
 bm = detector_error_model_to_check_matrices(dem, True)
 sampler = circuit.compile_detector_sampler()
-myDecoder = UFCLN(dem, d=12)
-transfer_mat = sio.loadmat('transfermatrices/transferMatrixcodel12m6.mat')['transfMat']
-transfer_mat_T = transfer_mat.transpose()
+myDecoder = UFCLN(dem, d=d)
 bpbpotf_cpp = BPOTF.OBPOTF(dem, p, BPOTF.OBPOTF.NoiseType.E_CLN, transfer_mat.astype('uint8'))
 bposd = bposd_decoder(
     bm.check_matrix,
@@ -55,6 +83,8 @@ bposd = bposd_decoder(
     max_iter = 100,
     osd_method = "osd_0"
 )
+process = psutil.Process(os.getpid())
+print(f"Memory used: {process.memory_info().rss / 1024 ** 2:.2f} MB")  # Memory in MB
 
 detection_events, observable_flips = sampler.sample(NMC, separate_observables=True)
 Pl_bposd = 0
@@ -63,14 +93,23 @@ Pl_cpp_otf = 0
 
 # detection_events = np.concatenate([detection_events, error_case_syndrome])
 # observable_flips = np.concatenate([observable_flips, error_case_observable])
+# mdic = {'demPcm' : bm.check_matrix.toarray(), "label": " demPcm"}
+# sio.savemat("dem_pcm.mat", mdic)
 
 sum_ton = 0
 sum_cpp = 0
 sum_bposd = 0
 worst_times = [0, 0, 0] # In this order, BPOSD, PyOTF, CppOTF
 stages_list = []
+updates = 0
 for index, detection_event in enumerate(detection_events):
     observable_flip = observable_flips[index]
+
+    finished = 100 * (index / NMC)
+
+    bposd_failed = False
+    py_otf_failed = False
+    cpp_otf_failed = False
 
     start_ton = timer()
     recovered_error, stage = myDecoder.decode(detection_event)#[0]
@@ -100,36 +139,61 @@ for index, detection_event in enumerate(detection_events):
 
     stages_list.append(stage)
 
-    if not np.all(recovered_error2 == recovered_error) or not np.all(recovered_error2 == recovered_error_cpp):
-        if not np.all(recovered_error2 == observable_flip):
-            bposd_failed = True
-            Pl_bposd += 1
-            print('BPOSD failed')
-        if not np.all(recovered_error == observable_flip):
-            py_otf_failed = True
-            Pl_py_otf += 1
-            print('PyOTF failed')
-        if not np.all(recovered_error_cpp == observable_flip):
-            cpp_otf_failed = True
-            Pl_cpp_otf += 1
-            print('CppOTF failed')
+    if not np.all(recovered_error2 == observable_flip):
+        bposd_failed = True
+        Pl_bposd += 1
+        # print('BPOSD failed')
+    if not np.all(recovered_error == observable_flip):
+        py_otf_failed = True
+        Pl_py_otf += 1
+        # print('PyOTF failed')
+    if not np.all(recovered_error_cpp == observable_flip):
+        cpp_otf_failed = True
+        Pl_cpp_otf += 1
+        # print('CppOTF failed')
 
+    if ((bposd_failed is True) or (py_otf_failed is True) or (cpp_otf_failed is True)) and PRINTING:
         print(f'Iteration number {index}')
         print(f'Pl BPOSD = {Pl_bposd/index}')
         print(f'Pl PyOTF = {Pl_py_otf/index}')
         print(f'Pl CppOTF = {Pl_cpp_otf/index}')
         print('\n')
+
+    if divmod(finished, 10) == (updates, 0):
+        updates += 1
+        print('Finished processing {} % of all events'.format(int(finished)))
+        process = psutil.Process(os.getpid())
+        print(f"Memory used: {process.memory_info().rss / 1024 ** 2:.2f} MB")  # Memory in MB
         
 
-print("Finished!")
-print(f"Final BPOSD: {Pl_bposd/NMC}")
-print(f"Final PyOTF: {Pl_py_otf/NMC}")
-print(f"Final CppOTF: {Pl_cpp_otf/NMC}")
 if (1 in stages_list) and (2 in stages_list) and (3 in stages_list):
     print("All stages tested!")
 else:
     print("Not all stages tested...")
 
+pe_bposd = Pl_bposd/NMC
+ped_bposd = pe_bposd/d
+pe_py_otf = Pl_py_otf/NMC
+ped_py_otf = pe_py_otf/d
+pe_cpp_otf = Pl_cpp_otf/NMC
+ped_cpp_otf = pe_cpp_otf/d
+
+print(f"Simulation data:")
+print(f" - p = {p}")
+print(f" - NMC = {NMC}")
+print(f" - d = {d}")
+print("")
+print(f"============ Probability results ============")
+print(f"BPOSD results:")
+print(f" * p(e): {pe_bposd}")
+print(f" * p(e)/d: {ped_bposd}")
+print(f"PyOTF results:")
+print(f" * p(e): {pe_py_otf}")
+print(f" * p(e)/d: {ped_py_otf}")
+print(f"CppOTF results:")
+print(f" * p(e): {pe_cpp_otf}")
+print(f" * p(e)/d: {ped_cpp_otf}")
+print("")
 print(f"============ Timing results ============")
 print(f"BPOSD times:")
 print(f" * Average time: {sum_bposd/NMC} s")
