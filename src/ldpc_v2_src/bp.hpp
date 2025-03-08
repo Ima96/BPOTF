b6934c28011ac48c7e31a3393fa07212c746acc1
Tonidmo <toni.demarti@gmail.com>
2025-03-08 22:56:53 +0100

    New change to bp.hpp file

--- src/ldpc_v2_src/bp.hpp
+++ src/ldpc_v2_src/bp.hpp
@@ -69,6 +69,7 @@ namespace ldpc {
       int iterations;
       int omp_thread_count;
       bool converge;
+      double epsilon;
       int random_schedule_seed;
       bool random_schedule_at_every_iteration;
       ldpc::rng::RandomListShuffle<int> rng_list_shuffle;
@@ -80,7 +81,7 @@ namespace ldpc {
                BpMethod bp_method = PRODUCT_SUM,
                BpSchedule schedule = PARALLEL,
                double min_sum_scaling_factor = 0.625,
-               double epsilon = 1e-14,
+               double _epsilon = 1e-14,
                int omp_threads = 1,
                const std::vector<int> &serial_schedule = NULL_INT_VECTOR,
                // TODO what should be default here? 0 is set but -1 is checked in decode method?
@@ -97,7 +98,7 @@ namespace ldpc {
          this->candidate_syndrome.resize(check_count);
          this->decoding.resize(bit_count);
          this->converge = 0;
-         this->epsilon = epsilon;
+         this->epsilon = _epsilon;
          this->omp_thread_count = omp_threads;
          this->random_schedule_seed = random_schedule_seed;
          this->random_schedule_at_every_iteration = random_schedule_at_every_iteration;
@@ -245,7 +246,8 @@ namespace ldpc {
                      // Clamping value to avoid extreme values near -1 or 1
                      double safe_value = std::clamp(e.check_to_bit_msg,  this->epsilon, 1.0 - this->epsilon);
                      e.check_to_bit_msg =
-                        message_sign * std::log((1 + e.check_to_bit_msg) / (1 - e.check_to_bit_msg));
+                        message_sign * std::log((1 + safe_value) / (1 - safe_value));
+                        // message_sign * std::log((1 + e.check_to_bit_msg) / (1 - e.check_to_bit_msg));
                      temp *= std::tanh(e.bit_to_check_msg / 2);
                   }
                }
