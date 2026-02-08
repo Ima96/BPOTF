190c03dfddef0f9eaac2931075dc2761c074b088
Tonidmo <toni.demarti@gmail.com>
2026-02-08 15:22:54 +0100

    Script changes to better input arguments in sinter bpotf class

--- test_scripts/DEM/sinter_bpotf.py
+++ test_scripts/DEM/sinter_bpotf.py
@@ -10,28 +10,41 @@ from ldpc.ckt_noise.dem_matrices import detector_error_model_to_check_matrices
 class SinterBpOtfDecoder(sinter.Decoder):
     """
     TODO: Insert here a description of what is this for.
+    TODO: It inputs too many arguments. Maybe we should place most of them in DemData.
     """
 
     def __init__(
             self,
-            p,
+            p, # TODO, no veo de que sirve tener la probabilidad aquí.
+            dem_data,
             noise_type = NoiseType.E_CLN,
             po_otf_csc_mat = None,
             po_ext_bp_iters = None,
-            ps_ext_dem_data = None,
             transfer_matrix = None,
             otf_matrix = None,
-            phen_check_matrix = None
+            phen_check_matrix = None,
+            phen_obs_matrix = None,
+            allow_undecomposed_hyperedges = True,
+            pcm = None
             ):
         
         self.m_p = p
         self.m_noise_type = noise_type
         self.m_po_otf_csc_mat = po_otf_csc_mat
         self.m_po_ext_bp_iters = po_ext_bp_iters
-        self.m_ps_ext_dem_data = ps_ext_dem_data
-        self.dem_data_phen_check_matrix = phen_check_matrix
-        self.transfer_matrix = transfer_matrix
-        self.otf_matrix = otf_matrix
+        self.m_dem_data = dem_data
+        # self.dem_data_phen_check_matrix = phen_check_matrix => In DemData
+        # self.transfer_matrix = transfer_matrix => In DemData
+        self.m_otf_matrix = otf_matrix
+        self.m_phen_obs_matrix = phen_obs_matrix
+        self.m_allow_undecomposed_hyperedges = allow_undecomposed_hyperedges
+        self.bpotf = OBPOTF(
+            pcm, 
+            self.m_p, 
+            self.m_noise_type,
+            ps_ext_dem_data=self.m_dem_data, 
+            po_ext_bp_iters= self.m_po_ext_bp_iters
+        )
 
     def decode_via_files(
             self,
@@ -46,28 +59,33 @@ class SinterBpOtfDecoder(sinter.Decoder):
             ) -> None:
 
         self.m_dem = stim.DetectorErrorModel.from_file(dem_path)
-        self.m_matrices = detector_error_model_to_check_matrices(self.m_dem, allow_undecomposed_hyperedges=True)
+        self.m_matrices = detector_error_model_to_check_matrices(
+            self.m_dem, 
+            allow_undecomposed_hyperedges=self.allow_undecomposed_hyperedges
+        )
         self.m_pcm = self.m_matrices.check_matrix
+        bm = detector_error_model_to_check_matrices(
+            self.m_dem, 
+            allow_undecomposed_hyperedges = self.m_allow_undecomposed_hyperedges
+        ) # Esto lo tendremos que cambiar
 
 
-        dem_data = DemData()
-        bm = detector_error_model_to_check_matrices(self.m_dem, allow_undecomposed_hyperedges = True)
-        dem_data.priors = bm.priors
-        dem_data.obs_matrix = bm.observables_matrix.toarray('F').astype(np.uint8)
+        # dem_data = DemData()
+        # dem_data.priors = bm.priors
+        # dem_data.obs_matrix = bm.observables_matrix.toarray('F').astype(np.uint8)
 
 
-        dem_data.phen_obs_matrix = bm.edge_observables_matrix.toarray('F').astype(np.uint8)
-            # Make the observables phenomenological matrix
-        dem_data.phen_check_matrix = self.dem_data_phen_check_matrix
-        dem_data.transfer_matrix = self.transfer_matrix
+        # dem_data.phen_obs_matrix = bm.edge_observables_matrix.toarray('F').astype(np.uint8)
+        #     # Make the observables phenomenological matrix
+        # dem_data.phen_check_matrix = self.dem_data_phen_check_matrix
+        # dem_data.transfer_matrix = self.transfer_matrix
 
         self.bpotf = OBPOTF(
-            self.m_pcm,
-            self.m_p,
+            bm.check_matrix, 
+            self.m_p, 
             self.m_noise_type,
-            self.m_po_otf_csc_mat,
-            self.m_po_ext_bp_iters,
-            self.m_ps_ext_dem_data
+            ps_ext_dem_data=self.m_dem_data, 
+            po_ext_bp_iters= self.m_po_ext_bp_iters
         )
 
         shots = stim.read_shot_data_file(path=dets_b8_in_path, format="b8", num_detectors=num_dets)
--- test_scripts/DEM/sinter_tests.py
+++ test_scripts/DEM/sinter_tests.py
@@ -2,28 +2,20 @@ import scipy.io as sio
 from scipy.sparse import csc_matrix
 from SlidingWindowDecoder.src.build_circuit import build_circuit
 from SlidingWindowDecoder.src.codes_q import create_bivariate_bicycle_codes#, create_circulant_matrix
-
-# from packaging.version import Version
-# from ldpc import __version__ as ldpc_version
-# ldpc_v2 = Version(ldpc_version) >= Version("2.0.0")
-# print("Using LDPC version v{}".format(ldpc_version))
-# if ldpc_v2 is True:
-# 	from ldpc import BpDecoder as bp_decoder
-# 	from ldpc import BpOsdDecoder as bposd_decoder
-# else:
-# 	from ldpc import bp_decoder
-# 	from ldpc import bposd_decoder
+from beliefmatching import detector_error_model_to_check_matrices
+import numpy as np
 
 import sinter
 from BPOTF import __version__ as bpotf_version
 print(f"BPOTF version is v{bpotf_version}")
-from BPOTF import SinterBpOtfDecoder, NoiseType
+from BPOTF import NoiseType, DemData
+from sinter_bpotf import SinterBpOtfDecoder
 
 #TODO -> Remove unnecessary imports.
 #TODO -> This test should be done with surface codes.
 
 def main():
-	BB_TYPE =  72
+	BB_TYPE =  108
 
 	ps = [1e-3, 1.5e-3, 2e-3, 3e-3][::-1]
 	p = ps[0]
@@ -32,25 +24,46 @@ def main():
 		# [72, 12, 6] último número es el numero de rondas
 		code, A_list, B_list = create_bivariate_bicycle_codes(6, 6, [3], [1,2], [1,2], [3])
 		d = 6
-		transfer_mat = sio.loadmat('transfermatrices/transferMatrixcodel6m6.mat')['transfMat']
+		# transfer_mat = sio.loadmat('transfermatrices/transferMatrixcodel6m6.mat')['transfMat']
+		transfer_mat = sio.loadmat('transfermatrices/PhenoTransf72Test.mat')['transfMatDEMtoPheno']
+		phenoDEM = sio.loadmat('transfermatrices/PhenoTransf72Test.mat')['dem_pheno']
+		phenoOBS = sio.loadmat('transfermatrices/PhenoTransf72Test.mat')['obsphen']
+	elif BB_TYPE == 90:
+		# [[90,8,10]]
+		code, A_list, B_list = create_bivariate_bicycle_codes(15, 3, [9], [1,2], [2,7], [0])
+		d = 10
+		transfer_mat = sio.loadmat('transfermatrices/PhenoTransf90Test.mat')['transfMatDEMtoPheno']
+		phenoDEM = sio.loadmat('transfermatrices/PhenoTransf90Test.mat')['dem_pheno']
+		phenoOBS = sio.loadmat('transfermatrices/PhenoTransf90Test.mat')['obsphen']
 	elif BB_TYPE == 108:
 		# [108, 8, 10]
 		code, A_list, B_list = create_bivariate_bicycle_codes(9, 6, [3], [1,2], [1,2], [3])
 		d = 10
-		transfer_mat = sio.loadmat('transfermatrices/transferMatrixcodel9m6.mat')['transfMat']
+		# transfer_mat = sio.loadmat('transfermatrices/transferMatrixcodel9m6.mat')['transfMat']
+		transfer_mat = sio.loadmat('transfermatrices/PhenoTransf108Test.mat')['transfMatDEMtoPheno']
+		phenoDEM = sio.loadmat('transfermatrices/PhenoTransf108Test.mat')['dem_pheno']
+		phenoOBS = sio.loadmat('transfermatrices/PhenoTransf108Test.mat')['obsphen']
 	elif BB_TYPE == 144:
 		# [144, 12, 12]
 		code, A_list, B_list = create_bivariate_bicycle_codes(12, 6, [3], [1,2], [1,2], [3])
 		d = 12
-		transfer_mat = sio.loadmat('transfermatrices/transferMatrixcodel12m6.mat')['transfMat']
+		# transfer_mat = sio.loadmat('transfermatrices/transferMatrixcodel12m6.mat')['transfMat']
+		transfer_mat = sio.loadmat('transfermatrices/PhenoTransf144Test.mat')['transfMatDEMtoPheno']
+		phenoDEM = sio.loadmat('transfermatrices/PhenoTransf144Test.mat')['dem_pheno']
+		phenoOBS = sio.loadmat('transfermatrices/PhenoTransf144Test.mat')['obsphen']
 	elif BB_TYPE == 288:
 		#  [288, 12, 18]
 		code, A_list, B_list = create_bivariate_bicycle_codes(12, 12, [3], [2,7], [1,2], [3])
 		d = 18
 		# transfer_mat = sio.loadmat('transfermatrices/BB288TransfDemsObs.mat')['transfMat']
-		transfer_mat = csc_matrix(sio.loadmat('transfermatrices/BB288CSC.mat')['transfMat'])
+		# transfer_mat = csc_matrix(sio.loadmat('transfermatrices/BB288CSC.mat')['transfMat']).toarray('F')
+		transfer_mat = sio.loadmat('transfermatrices/PhenoDEMSparseNEW.mat')['transfMatDEMtoPheno'].toarray('F')
+		phenoDEM = sio.loadmat('transfermatrices/PhenoDEMSparseNEW.mat')['dem_pheno']
+		phenoOBS = sio.loadmat('transfermatrices/PhenoDEMSparseNEW.mat')['obsphen']
 	else:
 		raise Exception("No such option!")
+
+
 
 	ps = 3e-3
 	circuit = build_circuit(code, A_list, B_list, 
@@ -58,7 +71,16 @@ def main():
 									num_repeat=d, # usually set to code distance
 									z_basis=True,   # whether in the z-basis or x-basis
 									use_both=False, # whether use measurement results in both basis to decode one basis
-									)
+	)
+	dem = circuit.detector_error_model()
+	bm = detector_error_model_to_check_matrices(dem, allow_undecomposed_hyperedges = True)
+	
+	dem_data = DemData()
+	dem_data.priors = bm.priors
+	dem_data.obs_matrix = bm.observables_matrix.toarray('F').astype(np.uint8)
+	dem_data.transfer_matrix = transfer_mat.astype(np.uint8)
+	dem_data.phen_obs_matrix = phenoOBS
+	dem_data.phen_check_matrix = phenoDEM
 
 	max_iters_dem = 100
 	max_iters_sparse = 400
@@ -66,10 +88,23 @@ def main():
 
 	bp_iterations = [max_iters_dem, max_iters_sparse, max_iters_forest]
 
+########################################################################################################################
+
+	# LAS SIGUIENTES LINEAS LAS USO PARA DEBUGGEAR, BORRAR CUANDO SINTER FUNCIONE
+ 
+	sinter_BPOTF = SinterBpOtfDecoder(
+							ps, 
+							dem_data,
+							po_ext_bp_iters = bp_iterations,
+							pcm = bm.check_matrix
+							)
+########################################################################################################################
+
+
 	# TODO ADAPT sinter to next
 	task = sinter.Task(
 		circuit=circuit,
-		collection_options=sinter.CollectionOptions(max_shots=10, max_errors=10),
+		collection_options=sinter.CollectionOptions(max_shots=1_000_000, max_errors=1_000),
 	)
 
 	# iterations: 35, default and 800
@@ -79,10 +114,9 @@ def main():
 					decoders=["bpotf"],
 					custom_decoders={
 						"bpotf": SinterBpOtfDecoder(
-							p, 
-							NoiseType.E_CLN,
+							ps, 
+							dem_data,
 							po_ext_bp_iters = bp_iterations,
-							transfer_matrix = transfer_mat
 							)
 						},
 					print_progress=True,
